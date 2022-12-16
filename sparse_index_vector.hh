af994cb3b54fd1591d585a4412b32bf9521c13ac
Reed Hedges <reed@interreality.org>
2022-12-16 08:30:09 -0500

    sparse_index_vector: Rename "items" to "values", much clearer and matches usual naming in standard library. Add comment.

--- sparse_index_vector.hh
+++ sparse_index_vector.hh
@@ -8,38 +8,41 @@
 
 namespace rhm {
 
-/* std::vector but also stores separate set of arbitrary "sparse" indices or priorities for items that determines its order. Items may have the same ordering index.  Use this instead of a std::map<size_t, item> or std::multimap<size_t, item> if insertions or changes are not frequent.  E.g. you will be setting up a list of items at startup but will otherwise only be iterating over the list, or only with occasional additions and removals.  Insertions and removals require linear search of the indices (though it should be sorted and could have a more efficient search for the nearest value) Modifications may also result in reallocation of both the items and indices containers.
-  TODO an iterator adapter of some kind that creates and returns std::pair<size_t, ItemT>'s (in Aria this would really just be for compatability with existing std::maps and std::multimaps)
-  todo most accessors could be constexpr after C++20
+/** std::vector but also stores separate set of arbitrary "sparse" indices or priorities for values that determines its order. Indices do not need to be unique, they are used to order the values not identify them.  Use this instead of a std::map<size_t, value> or std::multimap<size_t, value> if insertions or changes are not frequent.  E.g. you will be setting up a list of values at startup but will otherwise only be iterating over the list, or only with occasional additions and removals.  Insertions and removals currently require linear search of the indices (could be changed in the future.).  Insertions and removals may also result in reallocation of both the values and indices containers (std::vector objects).
+  @todo an iterator adapter of some kind that creates and returns std::pair<size_t, ValueT>'s (in Aria this would really just be for compatability with existing std::maps and std::multimaps)
+  @todo most accessors could be made constexpr, if only supporting C++20+  (or multiple versions of them?)
+  @todo insert() does a linear search to find the right position to insert the new value. we could check the size of the vector and do
+   this for small vectors, and do a binary search of the indices for larger vectors.  Or what about a more parallel search (e.g. simd comparisons)?
 */
 
 
 template<typename T>
 class sparse_index_vector {
 public:
-  using ItemVecT = std::vector<T>;
-  using iterator = typename ItemVecT::iterator;
-  using const_iterator = typename ItemVecT::const_iterator;
-  using size_type = typename ItemVecT::size_type;
+  using ValueVecT = std::vector<T>;
+  using iterator = typename ValueVecT::iterator;
+  using const_iterator = typename ValueVecT::const_iterator;
+  using size_type = typename ValueVecT::size_type;
   using IndexT = size_t;
+  using IndexVecT = std::vector<IndexT>;
 
 private:
-  ItemVecT items;
-  std::vector<IndexT> indices;
+  ValueVecT values;
+  IndexVecT indices;
 
 public:
-  iterator begin() noexcept { return items.begin(); }
-  iterator begin() const noexcept { return items.begin(); }
-  const_iterator cbegin() const noexcept { return items.cbegin(); }
-  iterator end() noexcept { return items.end(); }
-  iterator end() const noexcept { return items.end(); }
-  const_iterator cend() const noexcept { return items.cend(); }
+  iterator begin() noexcept { return values.begin(); }
+  iterator begin() const noexcept { return values.begin(); }
+  const_iterator cbegin() const noexcept { return values.cbegin(); }
+  iterator end() noexcept { return values.end(); }
+  iterator end() const noexcept { return values.end(); }
+  const_iterator cend() const noexcept { return values.cend(); }
 
-  size_type size() const noexcept { return items.size(); }
+  size_type size() const noexcept { return values.size(); }
 
   void clear() noexcept
   {
-    items.clear();
+    values.clear();
     indices.clear();
   }
 
@@ -54,10 +57,10 @@ public:
     }
     
     auto dist = std::distance(indices.begin(), i);
-    auto d = items.begin();
+    auto d = values.begin();
     std::advance(d, dist);
     indices.insert(i, index);
-    return items.insert(d, value);
+    return values.insert(d, value);
   }
 
   // todo:
@@ -66,20 +69,20 @@ public:
   // }
 
   // todo:
-  void erase(const T& item)
+  void erase(const T& value)
   {
-    auto f = find(items.begin(), items.end(), item);
-    if(f == items.end()) return;
-    auto dist = std::distance(items.begin(), f);
-    items.erase(f);
+    auto f = find(values.begin(), values.end(), value);
+    if(f == values.end()) return;
+    auto dist = std::distance(values.begin(), f);
+    values.erase(f);
     auto e = indices.begin();
     std::advance(e, dist);
     indices.erase(e);
   }
 
-  iterator erase(const T* const item)
+  iterator erase(const T* const value)
   {
-    return erase(*item);
+    return erase(*value);
   }
 
   template<typename T_> friend std::ostream& operator<<(std::ostream& os, const sparse_index_vector<T_>& v);
@@ -88,12 +91,12 @@ public:
 template<typename T>
 std::ostream& operator<<(std::ostream& s, const sparse_index_vector<T>& v)
 {
-  for(auto item_i = v.items.cbegin(), index_i = v.indices.cbegin(); 
-      item_i != v.items.end(); ++item_i, ++index_i)
+  for(auto value_i = v.values.cbegin(), index_i = v.indices.cbegin(); 
+      value_i != v.values.end(); ++value_i, ++index_i)
   {
-    if(item_i != v.items.begin())
+    if(value_i != v.values.begin())
       s << ", "; 
-    s << "(" << *index_i << ": " << *item_i <<")";
+    s << "(" << *index_i << ": " << *value_i <<")";
   }
   return s;
 }
